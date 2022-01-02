3db4c6f50674259b339ea01ca4b7ddb0c492ad05
Proxymiity <dev@ayaya.red>
2022-01-03 07:55:39 +0900

    oopsie

--- pxyTools/__init__.py
+++ pxyTools/__init__.py
@@ -4,3 +4,4 @@ from .object_cache import ObjectCache
 from .php_cache import HTTPCacheInterface, MDCacheInterface
 from .round_robin import RoundRobin
 from .make_pdf import make_pdf
+from .http2sql import H2SInterface
