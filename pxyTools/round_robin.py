446f63941e535a8a105d663e8d0dbb213a6c7246
Proxymiity <dev@ayaya.red>
2022-01-10 19:10:05 +0900

    Changed RoundRobin behavior

--- pxyTools/round_robin.py
+++ pxyTools/round_robin.py
@@ -2,11 +2,11 @@ class RoundRobin:
     def __init__(self, items, name=None):
         self.name = name or "rr"
         self.items = items
-        self.next_item = 0
+        self.item = 0
 
     def next(self):
-        self.next_item = 0 if len(self.items) == self.next_item + 1 else self.next_item + 1
-        print(f"[{self.name}] #{self.next_item - 1}")
-        return self.items[self.next_item - 1]
+        self.item = 0 if len(self.items) == self.item + 1 else self.item + 1
+        print(f"[{self.name}] #{self.item}")
+        return self.items[self.item]
 
     n = next
