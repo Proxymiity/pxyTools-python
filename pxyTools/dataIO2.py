2a71bab8d7f4e75c7cb720a1407f7f1aa3177ec6
Proxymiity <dev@ayaya.red>
2022-06-21 03:38:56 +0900

    Added graceful_reload

--- pxyTools/dataIO2.py
+++ pxyTools/dataIO2.py
@@ -22,6 +22,17 @@ class JSONDict(dict):
     def reload(self):
         self.__init__(self.path, self.encoding)
 
+    def graceful_reload(self):
+        for k in self.copy():
+            self.pop(k)
+        if self.path.is_file():
+            with open(self.path, encoding=self.encoding, mode="r") as f:
+                j = json.load(f)
+        else:
+            j = {}
+        for k, v in j.items():
+            self[k] = v
+
 
 class JSONList(list):
     def __init__(self, path, encoding='utf-8', data=None):
@@ -42,3 +53,14 @@ class JSONList(list):
 
     def reload(self):
         self.__init__(self.path, self.encoding)
+
+    def graceful_reload(self):
+        for v in self.copy():
+            self.remove(v)
+        if self.path.is_file():
+            with open(self.path, encoding=self.encoding, mode="r") as f:
+                j = json.load(f)
+        else:
+            j = []
+        for v in j:
+            self.append(v)
